5ffbab3cb50684e25f3ff4ff6c0353b857d743cb
devendrk7 <devendrak9610@gmail.com>
2026-09-21 18:43:02 +0530

    Functionality(Load Postings for txt file) of loader.py is Done

--- loader.py
+++ loader.py
@@ -2,9 +2,13 @@ from pathlib import Path
 
 def load_postings(file_path):
     path = Path(file_path)
-    content = path.read_text(encoding="utf-8")
+    try :
+        content = path.read_text(encoding="utf-8-sig")
+    except FileNotFoundError:
+        print("Error: File not found.")
+        return []
     parts = content.split("----------")
     cleaned =[item.strip() for item in parts if item.strip()]
     return cleaned
-postings = load_postings("data/sample/postings.txt")
+
 
