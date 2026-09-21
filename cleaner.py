e598890f5b7fb610f30205813c789033580d7d6a
devendrk7 <devendrak9610@gmail.com>
2026-09-22 00:47:12 +0530

    Build cleaned DataFrame from postings

---
+++ cleaner.py
@@ -0,0 +1,50 @@
+
+import pandas as pd
+
+from extractors import (extract_title,extract_stipend,extract_pay_status,
+                        extract_duration,extract_work_mode,extract_skills,
+                        extract_location,extract_job_type)
+def build_record(posting):
+    title = extract_title(posting)
+    stipend = extract_stipend(posting)
+    pay_status = extract_pay_status(posting)
+    duration = extract_duration(posting)
+    work_mode = extract_work_mode(posting)
+    skills = extract_skills(posting)
+    location = extract_location(posting)
+    job_type = extract_job_type(posting)
+    if stipend:
+        stipendMin = stipend[0]
+        stipendMax = stipend[1]
+        stipendMid = stipend[2]
+    else:
+        stipendMin = None
+        stipendMax = None
+        stipendMid = None
+
+    return {
+    "title": title,
+    "stipend_max": stipendMax,
+    "stipend_min": stipendMin,
+    "stipend_mid": stipendMid,
+    "pay_status": pay_status,
+    "duration_weeks": round(duration, 1) if duration is not None else None,
+    "work_mode": work_mode,
+    "skills": skills,
+    "location": location,
+    "job_type": job_type
+    }
+def create_dataframe(postings):
+    records = []
+
+    for posting in postings:
+        record = build_record(posting)
+        records.append(record)
+
+    df = pd.DataFrame(records)
+
+    # df["stipend_min"] = pd.to_numeric(df["stipend_min"], errors="coerce")
+    # df["stipend_max"] = pd.to_numeric(df["stipend_max"], errors="coerce")
+    # df["stipend_mid"] = pd.to_numeric(df["stipend_mid"], errors="coerce")
+    return df
+
