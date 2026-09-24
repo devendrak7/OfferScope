5e0bc0f337798442c7ccbc4dfbef87c90d2f7039
devendrk7 <devendrak9610@gmail.com>
2026-09-24 23:50:51 +0530

    Terminal Base UI added

---
+++ ui.py
@@ -0,0 +1,15 @@
+def show_menu():
+    print("\n========== OFFERSCOPE ==========")
+    print("""   1. Load postings(Power Button)
+    2. Extraction quality report
+    3. Stipend statistics
+    4. Skill demand
+    5. Skill vs pay
+    6. Search / filter postings
+    7. Export CSV / JSON / TXT
+    8. Exit""")
+    print("================================")
+
+
+def get_choice():
+    return input("Enter your choice: ")
