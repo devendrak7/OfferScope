d7cbe3bff8bc44118fa3e672cd5bf91dc996bb29
devendrk7 <devendrak9610@gmail.com>
2026-09-25 22:20:14 +0530

    Minor Change

--- ui.py
+++ ui.py
@@ -1,15 +1,14 @@
 def show_menu():
     print("\n========== OFFERSCOPE ==========")
-    print("""   1. Load postings(Power Button)
-    2. Extraction quality report
-    3. Stipend statistics
-    4. Skill demand
-    5. Skill vs pay
-    6. Search / filter postings
-    7. Export CSV / JSON / TXT
-    8. Exit""")
+    print("1. Load postings")
+    print("2. Extraction quality report")
+    print("3. Stipend statistics")
+    print("4. Skill demand")
+    print("5. Skill vs pay")
+    print("6. Search / filter postings")
+    print("7. Export CSV / JSON / TXT")
+    print("8. Exit")
     print("================================")
-
 
 def get_choice():
     return input("Enter your choice: ").strip()
